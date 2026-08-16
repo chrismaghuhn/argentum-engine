b9ab88d16cf9f8351ead5c63cfabb3158bc7c39a
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-16 13:55:36 +0200

    test: constrain Command Tower color domain

--- mtg-sets/2008-2016/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/CommandTowerScenarioTest.kt
+++ mtg-sets/2008-2016/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/CommandTowerScenarioTest.kt
@@ -47,6 +47,7 @@ class CommandTowerScenarioTest : FunSpec({
 
         val decision = driver.pendingDecision.shouldBeInstanceOf<ChooseColorDecision>()
         decision.playerId shouldBe player
+        decision.availableColors shouldBe setOf(Color.BLACK, Color.GREEN)
         driver.submitDecision(player, ColorChosenResponse(decision.id, Color.GREEN)).isSuccess shouldBe true
 
         driver.isTapped(tower) shouldBe true
