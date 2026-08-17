c7a0d4a3af451ef95e2a1d2ce5587f8559a8e7a6
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:24:03 +0200

    feat(card): close Boros Charm

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c16/cards/BorosCharmReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c16.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Boros Charm reprint in Commander 2016. The canonical CardDefinition lives in GTC. */
+val BorosCharmReprint = Printing(
+    oracleId = "2679d0dd-ba30-4a1c-b6a0-b3ac6c790496",
+    name = "Boros Charm",
+    setCode = "C16",
+    collectorNumber = "185",
+    scryfallId = "dfa38f6a-177a-4f3a-9a05-325c715a94c5",
+    artist = "Zoltan Boros",
+    imageUri = "https://cards.scryfall.io/normal/front/d/f/dfa38f6a-177a-4f3a-9a05-325c715a94c5.jpg?1783937051",
+    releaseDate = "2016-11-11",
+    rarity = Rarity.UNCOMMON,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/cmr/cards/BorosCharmReprints.kt
@@ -0,0 +1,29 @@
+package com.wingedsheep.mtg.sets.definitions.cmr.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Boros Charm reprints in Commander Legends. The canonical CardDefinition lives in GTC. */
+val BorosCharmCmrDraftReprint = Printing(
+    oracleId = "2679d0dd-ba30-4a1c-b6a0-b3ac6c790496",
+    name = "Boros Charm",
+    setCode = "CMR",
+    collectorNumber = "442",
+    scryfallId = "36c3288b-ab10-40df-a0ce-17ee4ecdb98e",
+    artist = "Zoltan Boros",
+    imageUri = "https://cards.scryfall.io/normal/front/3/6/36c3288b-ab10-40df-a0ce-17ee4ecdb98e.jpg?1783928700",
+    releaseDate = "2020-11-20",
+    rarity = Rarity.UNCOMMON,
+)
+
+val BorosCharmCmrCommanderReprint = Printing(
+    oracleId = "2679d0dd-ba30-4a1c-b6a0-b3ac6c790496",
+    name = "Boros Charm",
+    setCode = "CMR",
+    collectorNumber = "687",
+    scryfallId = "607d5bb1-001a-451a-8021-434f7c0eda93",
+    artist = "Zoltan Boros",
+    imageUri = "https://cards.scryfall.io/normal/front/6/0/607d5bb1-001a-451a-8021-434f7c0eda93.jpg?1783928603",
+    releaseDate = "2020-11-20",
+    rarity = Rarity.UNCOMMON,
+)
