8e7cc889d2ce97cd4d212d82f27a04880c74b0a8
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:35:00 +0200

    feat(card): close Disenchant

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/znr/cards/DisenchantReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.znr.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Disenchant reprint in Zendikar Rising. Canonical CardDefinition lives in its earliest set.
+ */
+val DisenchantReprint = Printing(
+    oracleId = "a7e97fa9-4b72-4548-b854-5be5f18a6f1a",
+    name = "Disenchant",
+    setCode = "ZNR",
+    collectorNumber = "10",
+    scryfallId = "5c37ba56-e22c-4b10-821a-2827c5cd81ad",
+    artist = "Colin Boyer",
+    imageUri = "https://cards.scryfall.io/normal/front/5/c/5c37ba56-e22c-4b10-821a-2827c5cd81ad.jpg?1783929420",
+    releaseDate = "2020-09-25",
+    rarity = Rarity.COMMON,
+)
