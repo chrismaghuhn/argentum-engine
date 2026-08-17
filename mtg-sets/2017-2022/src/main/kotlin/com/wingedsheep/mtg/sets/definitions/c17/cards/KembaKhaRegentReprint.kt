295c63a5209028d4ed3972412e949312edd9bd36
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 11:25:53 +0200

    feat(card): add Kemba, Kha Regent

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c17/cards/KembaKhaRegentReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c17.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Kemba, Kha Regent reprint in C17. Canonical CardDefinition lives in SOM. */
+val KembaKhaRegentC17Reprint = Printing(
+    oracleId = "499d5801-0c56-40f2-9a46-77025ed6cb87",
+    name = "Kemba, Kha Regent",
+    setCode = "C17",
+    collectorNumber = "63",
+    scryfallId = "4876ec2f-6c54-41df-8a7b-9c33d952ff12",
+    artist = "Todd Lockwood",
+    imageUri = "https://cards.scryfall.io/normal/front/4/8/4876ec2f-6c54-41df-8a7b-9c33d952ff12.jpg?1783935928",
+    releaseDate = "2017-08-25",
+    rarity = Rarity.RARE,
+)
