b2739dc7a569d4660d0876cbc2a42b624d8561f1
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:08:22 +0200

    Close Deathreap and Night's Whisper printing catalog gaps

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/msc/cards/NightsWhisperReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.msc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Night's Whisper reprint in Marvel Super Heroes Commander. */
+val NightsWhisperReprint = Printing(
+    oracleId = "7ffae8f8-3006-4969-a339-6d30678f87ea",
+    name = "Night's Whisper",
+    setCode = "MSC",
+    collectorNumber = "158",
+    scryfallId = "1ed2b60b-5f20-4ae6-93fb-51540ad9c9e7",
+    artist = "Fariba Khamseh",
+    imageUri = "https://cards.scryfall.io/normal/front/1/e/1ed2b60b-5f20-4ae6-93fb-51540ad9c9e7.jpg?1783903240",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.COMMON,
+)
