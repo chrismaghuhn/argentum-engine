8437fea4c37ed48cca2847b6b5c6a3a862d362a0
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 20:57:20 +0200

    feat(card): close Danitha Capashen, Paragon printing coverage

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/j22/cards/DanithaCapashenParagonReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.j22.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Danitha Capashen, Paragon reprint in Jumpstart 2022. Canonical CardDefinition lives in Dominaria.
+ */
+val DanithaCapashenParagonReprint = Printing(
+    oracleId = "4b6377da-83e7-4519-9582-16a9c16b8faa",
+    name = "Danitha Capashen, Paragon",
+    setCode = "J22",
+    collectorNumber = "168",
+    scryfallId = "5569b514-9049-4b44-b69e-78552a7bbf97",
+    artist = "Chris Rallis",
+    imageUri = "https://cards.scryfall.io/normal/front/5/5/5569b514-9049-4b44-b69e-78552a7bbf97.jpg?1783919122",
+    releaseDate = "2022-12-02",
+    rarity = Rarity.UNCOMMON,
+)
