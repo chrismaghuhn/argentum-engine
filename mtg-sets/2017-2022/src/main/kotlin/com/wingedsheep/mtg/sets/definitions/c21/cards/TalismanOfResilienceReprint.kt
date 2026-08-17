6e52db8ee8d0967e36b7811486ee154ebcf0a93e
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:36:51 +0200

    feat(card): close Talisman of Resilience

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c21/cards/TalismanOfResilienceReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c21.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Talisman of Resilience reprint in Commander 2021. The canonical CardDefinition lives in MH1. */
+val TalismanOfResilienceReprint = Printing(
+    oracleId = "42b8aa14-10bc-4bd6-88d9-4bb287eadd19",
+    name = "Talisman of Resilience",
+    setCode = "C21",
+    collectorNumber = "270",
+    scryfallId = "6c7d1ad3-bf92-490e-968b-ebdea1ed9eb8",
+    artist = "Lindsey Look",
+    imageUri = "https://cards.scryfall.io/normal/front/6/c/6c7d1ad3-bf92-490e-968b-ebdea1ed9eb8.jpg?1783927502",
+    releaseDate = "2021-04-23",
+    rarity = Rarity.UNCOMMON,
+)
---
+++ mtg-sets/2024/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/blc/cards/TalismanOfResilienceReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.blc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Talisman of Resilience reprint in Bloomburrow Commander. The canonical CardDefinition lives in MH1. */
+val TalismanOfResilienceReprint = Printing(
+    oracleId = "42b8aa14-10bc-4bd6-88d9-4bb287eadd19",
+    name = "Talisman of Resilience",
+    setCode = "BLC",
+    collectorNumber = "288",
+    scryfallId = "49766e23-74f5-4d6a-a882-53127028face",
+    artist = "Lindsey Look",
+    imageUri = "https://cards.scryfall.io/normal/front/4/9/49766e23-74f5-4d6a-a882-53127028face.jpg?1783910645",
+    releaseDate = "2024-08-02",
+    rarity = Rarity.UNCOMMON,
+)
