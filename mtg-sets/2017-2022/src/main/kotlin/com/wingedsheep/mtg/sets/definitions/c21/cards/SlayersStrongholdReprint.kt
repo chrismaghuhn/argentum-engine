34266d104ebcaa6d6cfea0cc51050a927daad3bd
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 20:36:33 +0200

    feat(card): close Slayers' Stronghold printing coverage

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c21/cards/SlayersStrongholdReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c21.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Slayers' Stronghold reprint in Commander 2021. The canonical CardDefinition lives in AVR. */
+val SlayersStrongholdReprint = Printing(
+    oracleId = "2de7367b-a5a4-43f4-8f8f-b931ea28150d",
+    name = "Slayers' Stronghold",
+    setCode = "C21",
+    collectorNumber = "318",
+    scryfallId = "d72b33dd-942d-4374-8f9a-7382303bee2b",
+    artist = "Karl Kopinski",
+    imageUri = "https://cards.scryfall.io/normal/front/d/7/d72b33dd-942d-4374-8f9a-7382303bee2b.jpg?1783927482",
+    releaseDate = "2021-04-23",
+    rarity = Rarity.RARE,
+)
