1486ebb71c9471777a741684323a98a1f7b2516b
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 12:25:55 +0200

    feat(card): add Mother of Runes

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/clb/cards/MotherOfRunesReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.clb.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Mother of Runes reprint in Commander Legends: Battle for Baldur's Gate. */
+val MotherOfRunesReprint = Printing(
+    oracleId = "60433b48-d27f-413c-905c-43839b1943f1",
+    name = "Mother of Runes",
+    setCode = "CLB",
+    collectorNumber = "702",
+    scryfallId = "a5e19147-e459-43a6-8ef0-e37968a462e3",
+    artist = "Scott M. Fischer",
+    imageUri = "https://cards.scryfall.io/normal/front/a/5/a5e19147-e459-43a6-8ef0-e37968a462e3.jpg?1783922485",
+    releaseDate = "2022-06-10",
+    rarity = Rarity.UNCOMMON,
+)
