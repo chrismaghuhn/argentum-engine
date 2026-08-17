42281853415bd29696b2f7059c13d9a6c4576f06
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:53:33 +0200

    feat(card): close Swords to Plowshares

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/cmr/cards/SwordsToPlowsharesReprint.kt
@@ -0,0 +1,29 @@
+package com.wingedsheep.mtg.sets.definitions.cmr.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Swords to Plowshares reprints in Commander Legends. */
+val SwordsToPlowsharesCmr627Reprint = Printing(
+    oracleId = "b1544f21-7e98-461b-aed5-e748b0168c52",
+    name = "Swords to Plowshares",
+    setCode = "CMR",
+    collectorNumber = "627",
+    scryfallId = "cd33c0f5-5545-477a-9185-53c707983795",
+    artist = "Sam Wolfe Connelly",
+    imageUri = "https://cards.scryfall.io/normal/front/c/d/cd33c0f5-5545-477a-9185-53c707983795.jpg?1783928628",
+    releaseDate = "2020-11-20",
+    rarity = Rarity.UNCOMMON,
+)
+
+val SwordsToPlowsharesCmr387Reprint = Printing(
+    oracleId = "b1544f21-7e98-461b-aed5-e748b0168c52",
+    name = "Swords to Plowshares",
+    setCode = "CMR",
+    collectorNumber = "387",
+    scryfallId = "be2b4177-e47c-4dde-9ead-31b7602065ec",
+    artist = "Sam Wolfe Connelly",
+    imageUri = "https://cards.scryfall.io/normal/front/b/e/be2b4177-e47c-4dde-9ead-31b7602065ec.jpg?1783928726",
+    releaseDate = "2020-11-20",
+    rarity = Rarity.UNCOMMON,
+)
