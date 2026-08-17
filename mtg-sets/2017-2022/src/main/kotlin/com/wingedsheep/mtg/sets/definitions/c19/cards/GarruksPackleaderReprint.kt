45529a12d2de9d2c942b27306af0ff43a7ea2c27
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 21:23:04 +0200

    feat(card): close Garruk's Packleader printing coverage

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c19/cards/GarruksPackleaderReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.c19.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Garruk's Packleader reprint in C19. Canonical CardDefinition lives in M11.
+ */
+val GarruksPackleaderReprint = Printing(
+    oracleId = "13279222-422d-4447-9451-2463b0c714c6",
+    name = "Garruk's Packleader",
+    setCode = "C19",
+    collectorNumber = "168",
+    scryfallId = "dafeafa0-b937-4387-8626-449a810486b4",
+    artist = "Nils Hamm",
+    imageUri = "https://cards.scryfall.io/normal/front/d/a/dafeafa0-b937-4387-8626-449a810486b4.jpg?1783932745",
+    releaseDate = "2019-08-23",
+    rarity = Rarity.UNCOMMON,
+)
