9e491982cb3b3cb1aedf093881b5690fe6eb9353
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:35:03 +0200

    feat(card): close Talisman of Conviction

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/msc/cards/TalismanOfConvictionReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.msc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Talisman of Conviction reprint in Marvel Super Heroes Commander. The canonical CardDefinition lives in MH1. */
+val TalismanOfConvictionReprint = Printing(
+    oracleId = "6c326439-5620-4ec6-a56a-fe9c3d5d2a46",
+    name = "Talisman of Conviction",
+    setCode = "MSC",
+    collectorNumber = "217",
+    scryfallId = "a365885c-f7d9-4ea1-9125-5265055e5570",
+    artist = "Uzuri",
+    imageUri = "https://cards.scryfall.io/normal/front/a/3/a365885c-f7d9-4ea1-9125-5265055e5570.jpg?1783903211",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.UNCOMMON,
+)
