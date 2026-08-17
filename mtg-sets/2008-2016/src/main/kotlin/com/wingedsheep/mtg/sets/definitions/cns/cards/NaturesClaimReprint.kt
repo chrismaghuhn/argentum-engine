9dff46595e7e3eb71b711c0c3f5642eaee9c245d
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 20:40:46 +0200

    feat(card): close Nature's Claim printing coverage

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/cns/cards/NaturesClaimReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.cns.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Nature's Claim reprint in Conspiracy. The canonical CardDefinition lives in WWK. */
+val NaturesClaimReprint = Printing(
+    oracleId = "6d4e558e-9109-4918-a082-fdcbaffd516b",
+    name = "Nature's Claim",
+    setCode = "CNS",
+    collectorNumber = "171",
+    scryfallId = "51027197-55da-45a3-901b-5ccb204095e9",
+    artist = "Raoul Vitale",
+    imageUri = "https://cards.scryfall.io/normal/front/5/1/51027197-55da-45a3-901b-5ccb204095e9.jpg?1783939343",
+    releaseDate = "2014-06-06",
+    rarity = Rarity.COMMON,
+)
