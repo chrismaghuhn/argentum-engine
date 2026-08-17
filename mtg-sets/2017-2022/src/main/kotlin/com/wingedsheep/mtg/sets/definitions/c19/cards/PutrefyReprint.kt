ef8c1332f8bd1939e0b032b7a1b37fd97f766422
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 18:59:21 +0200

    feat(card): close Putrefy

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c19/cards/PutrefyReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c19.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Putrefy reprint in Commander 2019. The canonical CardDefinition lives in RAV. */
+val PutrefyReprint = Printing(
+    oracleId = "9b271430-f53d-42d6-a547-2f286dd9bcb6",
+    name = "Putrefy",
+    setCode = "C19",
+    collectorNumber = "197",
+    scryfallId = "4ff773ac-ba7a-45da-a912-07222f7bf781",
+    artist = "Igor Kieryuk",
+    imageUri = "https://cards.scryfall.io/normal/front/4/f/4ff773ac-ba7a-45da-a912-07222f7bf781.jpg?1783932733",
+    releaseDate = "2019-08-23",
+    rarity = Rarity.UNCOMMON,
+)
