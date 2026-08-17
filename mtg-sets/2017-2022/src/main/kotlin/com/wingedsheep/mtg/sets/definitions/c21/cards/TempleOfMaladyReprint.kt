37e6f5d49173b0874492c470553b5f8e3cf03bf7
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 20:49:40 +0200

    feat(card): close Temple of Malady printing coverage

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c21/cards/TempleOfMaladyReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c21.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Temple of Malady reprint in Commander 2021. Canonical CardDefinition lives in JOU. */
+val TempleOfMaladyReprint = Printing(
+    oracleId = "dc55421f-dee8-4263-9df0-2365df5f14bb",
+    name = "Temple of Malady",
+    setCode = "C21",
+    collectorNumber = "323",
+    scryfallId = "2ed1189f-427d-43d0-86af-20daec4143ef",
+    artist = "Titus Lunter",
+    imageUri = "https://cards.scryfall.io/normal/front/2/e/2ed1189f-427d-43d0-86af-20daec4143ef.jpg?1783927480",
+    releaseDate = "2021-04-23",
+    rarity = Rarity.RARE,
+)
