cdfecd9d08967dd3176f9db676b764c0e05c4678
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:53:03 +0200

    feat(card): close Loxodon Warhammer

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c17/cards/LoxodonWarhammerReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c17.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Loxodon Warhammer reprint in Commander 2017. The canonical CardDefinition lives in MRD. */
+val LoxodonWarhammerReprint = Printing(
+    oracleId = "dba35ac5-7ad3-488a-a006-6b9a1d54eea5",
+    name = "Loxodon Warhammer",
+    setCode = "C17",
+    collectorNumber = "216",
+    scryfallId = "08417d60-0896-40b4-8dab-f36c65df67df",
+    artist = "Jeremy Jarvis",
+    imageUri = "https://cards.scryfall.io/normal/front/0/8/08417d60-0896-40b4-8dab-f36c65df67df.jpg?1783935866",
+    releaseDate = "2017-08-25",
+    rarity = Rarity.UNCOMMON,
+)
