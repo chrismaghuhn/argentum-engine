967dd27b4dbbf75db35576005f1128784437e995
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:39:52 +0200

    feat(card): close Viscera Seer printing coverage

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c13/cards/SakuraTribeElderReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c13.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Sakura-Tribe Elder reprint in Commander 2013. The canonical CardDefinition lives in CHK. */
+val SakuraTribeElderReprint = Printing(
+    oracleId = "e3afc704-220f-498f-9eaa-0821b17dc24c",
+    name = "Sakura-Tribe Elder",
+    setCode = "C13",
+    collectorNumber = "168",
+    scryfallId = "662af92a-d931-4028-8a80-3ae21e137647",
+    artist = "Carl Critchlow",
+    imageUri = "https://cards.scryfall.io/normal/front/6/6/662af92a-d931-4028-8a80-3ae21e137647.jpg?1783939656",
+    releaseDate = "2013-11-01",
+    rarity = Rarity.COMMON,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c19/cards/SakuraTribeElderReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c19.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Sakura-Tribe Elder reprint in Commander 2019. The canonical CardDefinition lives in CHK. */
+val SakuraTribeElderReprint = Printing(
+    oracleId = "e3afc704-220f-498f-9eaa-0821b17dc24c",
+    name = "Sakura-Tribe Elder",
+    setCode = "C19",
+    collectorNumber = "177",
+    scryfallId = "114328dd-6c4f-4b6b-b9b7-dfa8c9b79495",
+    artist = "Ted Galaday",
+    imageUri = "https://cards.scryfall.io/normal/front/1/1/114328dd-6c4f-4b6b-b9b7-dfa8c9b79495.jpg?1783932742",
+    releaseDate = "2019-08-23",
+    rarity = Rarity.COMMON,
+)
