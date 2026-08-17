8a12c9626dc523f3d3b7e459606778032f533b49
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 18:54:31 +0200

    feat(card): close Golgari Rot Farm

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c21/cards/GolgariRotFarmReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c21.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Golgari Rot Farm reprint in Commander 2021. The canonical CardDefinition lives in RAV. */
+val GolgariRotFarmReprint = Printing(
+    oracleId = "1b301478-b14f-4ef8-94e6-9647d582eabe",
+    name = "Golgari Rot Farm",
+    setCode = "C21",
+    collectorNumber = "291",
+    scryfallId = "1759843b-05f1-4bc6-bfc3-4c486e5b2e3f",
+    artist = "John Avon",
+    imageUri = "https://cards.scryfall.io/normal/front/1/7/1759843b-05f1-4bc6-bfc3-4c486e5b2e3f.jpg?1783927494",
+    releaseDate = "2021-04-23",
+    rarity = Rarity.UNCOMMON,
+)
