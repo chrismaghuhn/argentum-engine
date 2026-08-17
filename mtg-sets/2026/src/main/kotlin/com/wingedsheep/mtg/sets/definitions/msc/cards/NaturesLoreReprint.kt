175411dff1f1f31e7ea1e3bcf61d8328b4828deb
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:33:15 +0200

    feat(card): close Nature's Lore

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/msc/cards/NaturesLoreReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.msc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Nature's Lore reprint in Marvel Super Heroes Commander. The canonical CardDefinition lives in ICE. */
+val NaturesLoreReprint = Printing(
+    oracleId = "78826359-fe63-44ad-adc4-a17ffcd710e4",
+    name = "Nature's Lore",
+    setCode = "MSC",
+    collectorNumber = "177",
+    scryfallId = "7afc4a15-5ff8-457a-aa62-803b2d78d912",
+    artist = "Mateus Manhanini",
+    imageUri = "https://cards.scryfall.io/normal/front/7/a/7afc4a15-5ff8-457a-aa62-803b2d78d912.jpg?1783903228",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.COMMON,
+)
