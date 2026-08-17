bbcb14b69c9bab7b4e40b9aebe77987a98b14945
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 21:24:45 +0200

    feat(card): close Regrowth printing coverage

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/mh1/cards/RegrowthReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.mh1.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Regrowth reprint in Modern Horizons. The canonical CardDefinition lives in LEA. */
+val RegrowthReprint = Printing(
+    oracleId = "e6e4a8bd-5c40-4654-8de1-0da9afed90fd",
+    name = "Regrowth",
+    setCode = "MH1",
+    collectorNumber = "175",
+    scryfallId = "d771fc5d-b9a1-4637-8241-3f54616b64af",
+    artist = "Dan Murayama Scott",
+    imageUri = "https://cards.scryfall.io/normal/front/d/7/d771fc5d-b9a1-4637-8241-3f54616b64af.jpg?1783933093",
+    releaseDate = "2019-06-14",
+    rarity = Rarity.UNCOMMON,
+)
