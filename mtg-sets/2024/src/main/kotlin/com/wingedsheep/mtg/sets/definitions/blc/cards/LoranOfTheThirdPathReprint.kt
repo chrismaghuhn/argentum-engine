b74651315e1dfe45727da1014583a6b1d8bc124a
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 10:44:28 +0200

    feat(card): add Loran of the Third Path

---
+++ mtg-sets/2024/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/blc/cards/LoranOfTheThirdPathReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.blc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Loran of the Third Path reprint in Bloomburrow Commander (BLC). */
+val LoranOfTheThirdPathReprint = Printing(
+    oracleId = "b3d81980-76f2-44e2-b1c9-01e30c726312",
+    name = "Loran of the Third Path",
+    setCode = "BLC",
+    collectorNumber = "143",
+    scryfallId = "647e6b32-c755-4700-b151-6a174dbb2bd0",
+    artist = "Steven Belledin",
+    imageUri = "https://cards.scryfall.io/normal/front/6/4/647e6b32-c755-4700-b151-6a174dbb2bd0.jpg?1783910691",
+    releaseDate = "2024-08-02",
+    rarity = Rarity.RARE,
+)
