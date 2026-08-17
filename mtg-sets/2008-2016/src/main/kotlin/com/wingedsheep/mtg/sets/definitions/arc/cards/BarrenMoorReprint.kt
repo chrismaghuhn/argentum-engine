00e0e6ee84305e1b2b0a9625e82df394b21716a4
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:24:44 +0200

    feat(card): close Barren Moor

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/arc/cards/BarrenMoorReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.arc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Barren Moor reprint in Archenemy. The canonical CardDefinition lives in ONS. */
+val BarrenMoorReprint = Printing(
+    oracleId = "326ba371-124c-4949-a048-3a0c8962e567",
+    name = "Barren Moor",
+    setCode = "ARC",
+    collectorNumber = "124",
+    scryfallId = "33995084-d546-41d0-9d63-d448d21d3e80",
+    artist = "Heather Hudson",
+    imageUri = "https://cards.scryfall.io/normal/front/3/3/33995084-d546-41d0-9d63-d448d21d3e80.jpg?1783941888",
+    releaseDate = "2010-06-18",
+    rarity = Rarity.COMMON,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c21/cards/BarrenMoorReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.c21.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Barren Moor reprint in Commander 2021. The canonical CardDefinition lives in ONS. */
+val BarrenMoorReprint = Printing(
+    oracleId = "326ba371-124c-4949-a048-3a0c8962e567",
+    name = "Barren Moor",
+    setCode = "C21",
+    collectorNumber = "277",
+    scryfallId = "b38d7666-0791-44b4-877e-8fd33ce008aa",
+    artist = "Heather Hudson",
+    imageUri = "https://cards.scryfall.io/normal/front/b/3/b38d7666-0791-44b4-877e-8fd33ce008aa.jpg?1783927499",
+    releaseDate = "2021-04-23",
+    rarity = Rarity.UNCOMMON,
+)
