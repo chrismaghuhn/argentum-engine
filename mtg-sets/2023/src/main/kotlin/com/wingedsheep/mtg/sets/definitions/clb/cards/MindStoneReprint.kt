c477479530ccd43fdfa7fa1fce7d15e700116c76
chrismaghuhn <chrisgarkisch@gmail.com>
2026-08-17 19:30:19 +0200

    feat(card): close Mind Stone

---
+++ mtg-sets/2023/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/clb/cards/MindStoneReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.clb.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Mind Stone reprint in Commander Legends: Battle for Baldur's Gate. The canonical CardDefinition lives in WTH. */
+val MindStoneReprint = Printing(
+    oracleId = "c97361b5-af16-4a7b-af85-a429dbaf4ad2",
+    name = "Mind Stone",
+    setCode = "CLB",
+    collectorNumber = "325",
+    scryfallId = "1969ddc0-ee6c-4c3d-a9dd-7f1c491609be",
+    artist = "Ioannis Fiore",
+    imageUri = "https://cards.scryfall.io/normal/front/1/9/1969ddc0-ee6c-4c3d-a9dd-7f1c491609be.jpg?1783922670",
+    releaseDate = "2022-06-10",
+    rarity = Rarity.UNCOMMON,
+)
